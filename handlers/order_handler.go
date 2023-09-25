14d212b46168e8a2f7e52a7ac15b33c3c60f1a55
MohamadAlgazli <brawjym@gmail.com>
2023-09-25 22:11:34 +0300

    structuring project

---
+++ handlers/order_handler.go
@@ -0,0 +1,25 @@
+package handlers
+
+import "net/http"
+
+type Order struct {}
+
+func (o *Order) Create(w http.ResponseWriter, r *http.Request) {
+		
+}
+
+func (o *Order) List(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (o *Order) GetByID(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (o *Order) UpdateByID(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (o *Order) DeleteByID(w http.ResponseWriter, r *http.Request) {
+
+}
--- main.go
+++ main.go
@@ -1,41 +1,18 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"os"
+	"context"
+	"fmt"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-	"github.com/go-chi/cors"
-	"github.com/joho/godotenv"
+	"github.com/mohammad-Gazali/orders-api/application"
 )
 
 func main() {
-	godotenv.Load()
+	app := application.New()
 
-	router := chi.NewRouter()
-
-	router.Use(middleware.Logger)
-	router.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"https://*", "http://*"},
-		AllowedMethods:   []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"*"},
-		ExposedHeaders:   []string{"Link"},
-		AllowCredentials: false,
-		MaxAge:           300,
-	}))
-
-	server := &http.Server{
-		Handler: router,
-		Addr:    ":" + os.Getenv("PORT"),
-	}
-
-	log.Println("Server is Running on port: " + os.Getenv("PORT"))
-	
-	err := server.ListenAndServe()
+	err := app.Start(context.TODO())
 
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("failed to start app:", err)
 	}
 }
